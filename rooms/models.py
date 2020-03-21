3cf4577644a2b28b153146582eda193535bc6e88
TonyK <sunghk0901@gmail.com>
2020-03-21 16:59:20 -0700

    created roomtype selection

--- rooms/models.py
+++ rooms/models.py
@@ -4,9 +4,21 @@ from core import models as core_models
 from users import models as user_models
 
 # Create your models here.
+class AbstractItem(core_models.TimeStampedModel):
+    """Abstract Item"""
+
+    name = models.CharField(max_length=80)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
 
 
-class Room(core_models.TimeStampedModel):
+class RoomType(AbstractItem):
+    pass
+
 
     """ Room Model Definition """
 
@@ -24,3 +36,7 @@ class Room(core_models.TimeStampedModel):
     check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
     host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
+    room_type = models.ManyToManyField(RoomType, blank=True)
+
+    def __str__(self):
+        return self.name

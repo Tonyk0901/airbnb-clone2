18ddb1cdcd178ded87646760af192a6158d76c0d
TonyK <sunghk0901@gmail.com>
2020-03-22 22:45:13 -0700

    created custom commands that add test data.

---
+++ rooms/management/commands/seed_facilities.py
@@ -0,0 +1,17 @@
+from django.core.management.base import BaseCommand
+from rooms import models as room_model
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        facilities = [
+            "Private entrance",
+            "Paid parking on premises",
+            "Paid parking off premises",
+            "Elevator",
+            "Parking",
+            "Gym",
+        ]
+        for f in facilities:
+            room_model.Facility.objects.create(name=f)
+        self.stdout.write(self.style.SUCCESS(f"{len(facilities)} facilities created."))

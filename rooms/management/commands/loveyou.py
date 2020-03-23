08b8c6e708afa1ad8d453e493afe39773bbb132e
TonyK <sunghk0901@gmail.com>
2020-03-22 22:18:46 -0700

    succeeded in making the loveyou command to work in the way I want

--- rooms/management/commands/loveyou.py
+++ rooms/management/commands/loveyou.py
@@ -2,13 +2,12 @@ from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
-    help = "This command tells you that he loves me"
-
     def add_arguments(self, parser):
         parser.add_argument(
             "--times", help="How many times do you want me to tell you I love you"
         )
 
     def handle(self, *args, **options):
-        print(args, options)
-        print("i love you ")
+        times = options.get("times")
+        for t in range(0, int(times)):
+            self.stdout.write(self.style.SUCCESS("I love you."))

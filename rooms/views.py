7e78f90a16e6c712688c6b22b586ea2b8c0846bf
TonyK <sunghk0901@gmail.com>
2020-03-24 02:54:35 -0700

    printing out the main page contents. 10 rooms

--- rooms/views.py
+++ rooms/views.py
@@ -1,7 +1,18 @@
+from math import ceil
 from django.shortcuts import render
 from . import models
 
 
 def all_rooms(request):
-    all_rooms = models.Room.objects.all()
-    return render(request, "all_rooms.html", context={"rooms": all_rooms})
+    page = request.GET.get("page", 1)
+    page = int(page or 1)
+    page_size = 10
+    page_limit = page_size * page
+    page_offset = page_limit - page_size
+    all_rooms = models.Room.objects.all()[page_offset:page_limit]
+    page_count = ceil(models.Room.objects.count() / page_size)
+    return render(
+        request,
+        "rooms/home.html",
+        context={"rooms": all_rooms, "page": page, "page_count": page_count},
+    )

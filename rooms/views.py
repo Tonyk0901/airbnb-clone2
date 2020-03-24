5d43e1f72c9925bb0adcd48935b748bf40305f63
TonyK <sunghk0901@gmail.com>
2020-03-24 03:04:20 -0700

    created page navigator

--- rooms/views.py
+++ rooms/views.py
@@ -11,8 +11,14 @@ def all_rooms(request):
     page_offset = page_limit - page_size
     all_rooms = models.Room.objects.all()[page_offset:page_limit]
     page_count = ceil(models.Room.objects.count() / page_size)
+    page_range = range(1, page_count + 1)
     return render(
         request,
         "rooms/home.html",
-        context={"rooms": all_rooms, "page": page, "page_count": page_count},
+        context={
+            "rooms": all_rooms,
+            "page": page,
+            "page_count": page_count,
+            "page_range": page_range,
+        },
     )

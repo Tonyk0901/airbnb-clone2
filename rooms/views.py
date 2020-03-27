f76c8f0edf0741d8b8ab7b418018c0313a7aaff5
TonyK <sunghk0901@gmail.com>
2020-03-26 23:48:32 -0700

    learned to use the namespace and name for connecting urls

--- rooms/views.py
+++ rooms/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import ListView
+from django.shortcuts import render
 from . import models
 
 
@@ -6,6 +7,11 @@ class HomeView(ListView):
     """ Home View Definitino """
 
     model = models.Room
+    context_object_name = "rooms"
     paginate_by = 10
     paginate_orphans = 5
     ordering = "created"
+
+
+def room_detail(request, pk):
+    return render(request, "rooms/detail.html")

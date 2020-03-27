42a7a0671fd8e8966f10768b1f5fc3cb74785daa
TonyK <sunghk0901@gmail.com>
2020-03-26 21:48:18 -0700

    Found even simpler way to create the views by using the provided class ListView

--- rooms/views.py
+++ rooms/views.py
@@ -1,12 +1,11 @@
-from math import ceil
-from django.shortcuts import render
-from django.core.paginator import Paginator
+from django.views.generic import ListView
 from . import models
 
 
-def all_rooms(request):
-    page = request.GET.get("page")
-    room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10)
-    rooms = paginator.get_page(page)
-    return render(request, "rooms/home.html", context={"rooms": rooms,},)
+class HomeView(ListView):
+    """ Home View Definitino """
+
+    model = models.Room
+    paginate_by = 10
+    paginate_orphans = 5
+    ordering = "created"
